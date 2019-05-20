f342111309bc3aa1338b95d1a233b5bcc0454f27
Jeff Lapp <lappjeff20@gmail.com>
2019-05-20 17:21:19 -0400

    listAllCourses test passes successfully

--- school/src/test/java/com/lambdaschool/school/SchoolApplicationTests.java
+++ school/src/test/java/com/lambdaschool/school/SchoolApplicationTests.java
@@ -1,11 +1,7 @@
 package com.lambdaschool.school;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootApplication
 public class SchoolApplicationTests
@@ -13,7 +9,7 @@ public class SchoolApplicationTests
 
     public static void main(String[] args)
     {
-        SpringApplication.run(SchoolApplication.class, args);
+        SpringApplication.run(SchoolApplicationTests.class, args);
     }
 
 }
--- school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
@@ -1,5 +1,6 @@
 package com.lambdaschool.school.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.school.model.Course;
 import com.lambdaschool.school.model.Instructor;
 import com.lambdaschool.school.model.Student;
@@ -7,15 +8,19 @@ import com.lambdaschool.school.service.CourseService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static junit.framework.TestCase.assertEquals;
 
 @WebMvcTest(value = CourseController.class)
 class CourseControllerTest
@@ -50,8 +55,22 @@ class CourseControllerTest
 	}
 
 	@Test
-	void listAllCourses()
+	void listAllCourses() throws Exception
 	{
+		String url = "/courses/courses";
+
+		Mockito.when(courseService.findAll()).thenReturn(courses);
+
+		RequestBuilder rb = MockMvcRequestBuilders.get(url).accept(MediaType.APPLICATION_JSON);
+
+		MvcResult r = mockMvc.perform(rb).andReturn();
+
+		String stringResult = r.getResponse().getContentAsString();
+
+		ObjectMapper mapper = new ObjectMapper();
+		String expectedResult = mapper.writeValueAsString(courses);
+
+		assertEquals(expectedResult, stringResult);
 	}
 
 	@Test

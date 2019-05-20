630d856c44b0be8bc6ecec24fdaec1dcf0aaa1c9
Jeff Lapp <lappjeff20@gmail.com>
2019-05-20 17:04:28 -0400

    Created seed data for controller tests

--- school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
@@ -1,5 +1,8 @@
 package com.lambdaschool.school.controller;
 
+import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Instructor;
+import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.CourseService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,10 +27,21 @@ class CourseControllerTest
 	@MockBean
 	private CourseService courseService;
 
+	private ArrayList<Course> courses = new ArrayList<>();
 
 	@BeforeEach
 	void setUp()
 	{
+		Instructor instructor1 = new Instructor("Walter White");
+
+		Course course1 = new Course("Advanced Meth-Matics");
+
+		Student stud1 = new Student("Jesse Pinkman");
+
+		course1.getStudents().add(stud1);
+		instructor1.getCourses().add(course1);
+
+		courses.add(course1);
 	}
 
 	@AfterEach
